88831879db913db463d96e71b5df5f754896dcfa
nataliegarate <natalieegarate@hotmail.com>
2019-10-18 14:38:56 -0400

    updated to include post model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,5 +1,5 @@
 const User = require('./user')
-
+const Post = require('./post')
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -13,6 +13,11 @@ const User = require('./user')
  * for example, we can say: const {User} = require('../db/models')
  * instead of: const User = require('../db/models/user')
  */
+
+User.hasMany(Post)
+Post.belongsTo(User)
+
 module.exports = {
-  User
+  User,
+  Post
 }
