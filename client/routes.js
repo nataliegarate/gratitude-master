cd93125e1f6ff6ac79a8883de3410c3c50195f18
nataliegarate <natalieegarate@hotmail.com>
2019-10-18 15:53:47 -0400

    removed login display as fallback

--- client/routes.js
+++ client/routes.js
@@ -19,16 +19,15 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
+
+        <Route path="/login" component={Login} />
         {isLoggedIn && (
           <Switch>
             {/* Routes placed here are only available after logging in */}
             <Route path="/home" component={UserHome} />
           </Switch>
         )}
-        {/* Displays our Login component as a fallback */}
-        <Route component={Login} />
       </Switch>
     )
   }
