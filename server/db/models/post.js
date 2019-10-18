16a44b15bd864520f413ae57c5c3d5d8af6f7302
nataliegarate <natalieegarate@hotmail.com>
2019-10-18 17:07:49 -0400

    deleted title section from post model

--- server/db/models/post.js
+++ server/db/models/post.js
@@ -2,10 +2,6 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Post = db.define('posts', {
-  title: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
   content: {
     type: Sequelize.TEXT,
     allowNull: false
