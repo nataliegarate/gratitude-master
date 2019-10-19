8178c10fa5a882b38e71fdb4ff411353717b2e89
nataliegarate <natalieegarate@hotmail.com>
2019-10-18 20:51:30 -0400

    added get route for posts

--- server/api/posts.js
+++ server/api/posts.js
@@ -2,6 +2,22 @@ const router = require('express').Router()
 const Post = require('../db/models/post')
 module.exports = router
 
+//GET route to display user's posts
+
+router.get('/', async (req, res, next) => {
+  try {
+    // req.body.userId = req.user.id
+    const userPosts = await Post.findAll({
+      where: {
+        userId: req.user.id
+      }
+    })
+    res.json(userPosts)
+  } catch (err) {
+    next(err)
+  }
+})
+
 //POST route to add a new post
 router.post('/', async (req, res, next) => {
   try {
