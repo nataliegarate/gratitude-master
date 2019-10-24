ccd05a64a0cea13f474e7fcc39f8cb75e15aeea4
nataliegarate <natalieegarate@hotmail.com>
2019-10-24 19:47:23 -0400

    updated posts

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,4 @@
 const router = require('express').Router()
-module.exports = router
 
 router.use('/users', require('./users'))
 
@@ -10,3 +9,5 @@ router.use((req, res, next) => {
   error.status = 404
   next(error)
 })
+
+module.exports = router
